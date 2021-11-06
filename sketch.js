e7a859e7f279e7a7999ade7055ab3c7379b3cc1e
Simi234 <90729301+Simi234@users.noreply.github.com>
2021-11-06 10:00:13 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+function preload()
+{
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+        var balls_option={ 
+		isStatic:false,
+		restitution:0.3,
+		friction:0,
+		density:1.2
+		}
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	ground = new Ground(400,690,800,20);
+  right = new Ground(790,400,20,800);
+  left = new Ground(10,400,20,800);
+  top_wall = new Ground(400,100,800,20);
+  bin_1 = new Ground(650,700, 12,100);
+	bin_2 = new Ground(700,700,12,50);
+ball = Bodies.circle(100, 600, 10, balls_option);
+
+  World.add(world,ball);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ground.display();
+  right.display();
+  left.display();
+  top.display();
+  bin_1.display();
+  bin_2.display();
+  ellipse(ball.position.x,ball.position.y, 20)
+  drawSprites();
+ 
+}
+
+
+
